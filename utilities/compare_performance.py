5cc4f600a83a908bcac326ef73c101fb1ba26ba5
davidtey <64002422+davidtey@users.noreply.github.com>
2023-09-30 10:22:11 +0200

    initialise utility files

---
+++ utilities/compare_performance.py
@@ -0,0 +1,267 @@
+import os
+import sys
+sys.path.append("../")
+
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import time
+from pynverse import inversefunc
+from solvers.galerkin import DGMNet
+from solvers.bsde import BSDENet
+from solvers.branch import Net
+
+# performance evaluation for PDE
+def compare_performance(
+    grid_1d,
+    grid_nd,
+    grid_with_t,
+    t_lo,
+    true,
+    terminal,
+    fname=None,
+    debug_mode=False,
+    disable_galerkin=False,
+    disable_bsde=False,
+    disable_branch=False,
+    seeds=list(range(10)),
+    disable_relative=False,
+    **kwargs,
+):
+    if not os.path.isdir('../logs/data/'):
+        os.makedirs('../logs/data/')
+    if not os.path.isdir('../logs/plot/'):
+        os.makedirs('../logs/plot/')
+    x_lo, x_hi = grid_nd.min(), grid_nd.max()
+    grid_nd = grid_nd.astype(np.float32)
+    grid_with_t = grid_with_t.astype(np.float32)
+    
+    problem_name = fname
+    fname = f"{fname}_dim_{grid_nd.shape[0]}" if fname is not None else None
+    counter = 1
+
+    branch_runtime, branch_l1_error, branch_l2_error, branch_rel_error, branch = (
+        [],
+        [],
+        [],
+        [],
+        0,
+    )
+    branch_fdb_runtime, branch_total_runtime = [], []
+    dgm_runtime, dgm_l1_error, dgm_l2_error, dgm_rel_error, dgm = [], [], [], [], 0
+    bsde_runtime, bsde_l1_error, bsde_l2_error, bsde_rel_error, bsde = [], [], [], [], 0
+    for seed in seeds:
+        ######### Deep branching method
+        if not disable_branch:
+            torch.manual_seed(seed)
+            print(
+                f"#### Starting deep branching method with seed {seed} - {counter} out of {len(seeds)} times"
+            )
+            start = time.time()
+            model = Net(t_lo=t_lo, t_hi=t_lo, x_lo=x_lo, x_hi=x_hi, problem_name=problem_name, **kwargs)
+            model.train_and_eval(debug_mode)
+            branch_total_runtime.append(time.time() - start)
+            branch_fdb_runtime.append(model.fdb_runtime)
+            branch_runtime.append(branch_total_runtime[-1] - branch_fdb_runtime[-1])
+            # note that branch output batch_size x 1 instead of batch_size like other algo
+            tmp = (
+                model(torch.tensor(grid_with_t.T, device=device), patch=model.patches - 1)[:, 0].detach().cpu().numpy()
+            )
+            branch += tmp
+            branch_l1_error.append((np.abs(tmp - true)).mean())
+            branch_l2_error.append(((tmp - true) ** 2).mean())
+            if not disable_relative:
+                branch_rel_error.append((np.abs(tmp - true) / np.abs(true)).mean())
+            else:
+                branch_rel_error.append(0)
+            print(
+                f"Time taken: {branch_runtime[-1]:.0f} seconds; L1 error: {branch_l1_error[-1]:.2E}; L2 error: {branch_l2_error[-1]:.2E}; rel error: {branch_rel_error[-1]:.2E}."
+            )
+            print(
+                f"fdb runtime: {branch_fdb_runtime[-1]:.0f} seconds; total runtime: {branch_total_runtime[-1]:.0f} seconds."
+            )
+
+        ######### Deep galerkin method
+        if not disable_galerkin:
+            torch.manual_seed(seed)
+            print(
+                f"#### Starting deep galerkin method with seed {seed} - {counter} out of {len(seeds)} times"
+            )
+            start = time.time()
+            model = DGMNet(t_lo=t_lo, t_hi=kwargs["T"], x_lo=x_lo, x_hi=x_hi, **kwargs)
+            model.train_and_eval(debug_mode)
+            dgm_runtime.append(time.time() - start)
+            tmp = (
+                model(torch.tensor(grid_with_t.T, device=device)).detach().cpu().numpy()
+            )
+            dgm += tmp
+            dgm_l1_error.append((np.abs(tmp - true)).mean())
+            dgm_l2_error.append(((tmp - true) ** 2).mean())
+            if not disable_relative:
+                dgm_rel_error.append((np.abs(tmp - true) / np.abs(true)).mean())
+            else:
+                dgm_rel_error.append(0)
+            print(
+                f"Time taken: {dgm_runtime[-1]:.0f} seconds; L1 error: {dgm_l1_error[-1]:.2E}; L2 error: {dgm_l2_error[-1]:.2E}; rel error: {dgm_rel_error[-1]:.2E}."
+            )
+
+        ######### Deep BSDE method
+        if not disable_bsde:
+            torch.manual_seed(seed)
+            print(
+                f"#### Starting deep BSDE method with seed {seed} - {counter} out of {len(seeds)} times"
+            )
+            start = time.time()
+            model = BSDENet(t_lo=t_lo, t_hi=kwargs["T"], x_lo=x_lo, x_hi=x_hi, **kwargs)
+            model.train_and_eval(debug_mode)
+            bsde_runtime.append(time.time() - start)
+            tmp = (
+                model(torch.tensor(grid_nd.T, device=device))
+                .squeeze(-1)
+                .detach()
+                .cpu()
+                .numpy()
+            )
+            bsde += tmp
+            bsde_l1_error.append((np.abs(tmp - true)).mean())
+            bsde_l2_error.append(((tmp - true) ** 2).mean())
+            if not disable_relative:
+                bsde_rel_error.append((np.abs(tmp - true) / np.abs(true)).mean())
+            else:
+                bsde_rel_error.append(0)
+            print(
+                f"Time taken: {bsde_runtime[-1]:.0f} seconds; L1 error: {bsde_l1_error[-1]:.2E}; L2 error: {bsde_l2_error[-1]:.2E}; rel error: {bsde_rel_error[-1]:.2E}."
+            )
+
+        print("\n")
+        counter += 1
+
+    # Plot
+    f = plt.figure()
+    plt.plot(grid_1d, true, label="True solution")
+    plt.plot(grid_1d, terminal, label="Terminal condition")
+    # save into csv
+    if fname is not None:
+        plt_data = np.stack((grid_1d, true, terminal), axis=-1)
+        plt_header = "x, true, terminal"
+
+    if not disable_branch:
+        branch /= len(seeds)
+        branch_runtime, branch_l1_error, branch_l2_error, branch_rel_error = (
+            np.array(branch_runtime),
+            np.array(branch_l1_error),
+            np.array(branch_l2_error),
+            np.array(branch_rel_error),
+        )
+        branch_fdb_runtime, branch_total_runtime = np.array(
+            branch_fdb_runtime
+        ), np.array(branch_total_runtime)
+        print("\n")
+        print("######## Summary for deep branching method")
+        print(f"runtime:        {branch_runtime.mean():.0f}")
+        print(f"fdb runtime:    {branch_fdb_runtime.mean():.0f}")
+        print(f"total runtime:  {branch_total_runtime.mean():.0f}")
+        print(f"mean L1 error:  {branch_l1_error.mean():.2E}")
+        print(f"std L1 error:   {branch_l1_error.std():.2E}")
+        print(f"mean L2 error:  {branch_l2_error.mean():.2E}")
+        print(f"std L2 error:   {branch_l2_error.std():.2E}")
+        print(f"mean rel error: {branch_rel_error.mean():.2E}")
+        print(f"std rel error:  {branch_rel_error.std():.2E}")
+        # Plot
+        plt.plot(grid_1d, branch, label="Deep branching")
+        # save into csv
+        if fname is not None:
+            plt_data = np.concatenate(
+                (plt_data, np.expand_dims(branch, axis=-1)), axis=-1
+            )
+            plt_header += ", branch"
+            data = np.stack(
+                (branch_runtime, branch_l1_error, branch_l2_error, branch_rel_error),
+                axis=-1,
+            )
+            np.savetxt(
+                "../logs/data/branch_" + fname + ".csv",
+                data,
+                delimiter=",",
+                header="runtime,l1_error,l2_error,rel_error",
+                comments="",
+            )
+
+    if not disable_galerkin:
+        dgm /= len(seeds)
+        dgm_runtime, dgm_l1_error, dgm_l2_error, dgm_rel_error = (
+            np.array(dgm_runtime),
+            np.array(dgm_l1_error),
+            np.array(dgm_l2_error),
+            np.array(dgm_rel_error),
+        )
+        print("\n")
+        print("######## Summary for deep galerkin method")
+        print(f"runtime:        {dgm_runtime.mean():.0f}")
+        print(f"mean L1 error:  {dgm_l1_error.mean():.2E}")
+        print(f"std L1 error:   {dgm_l1_error.std():.2E}")
+        print(f"mean L2 error:  {dgm_l2_error.mean():.2E}")
+        print(f"std L2 error:   {dgm_l2_error.std():.2E}")
+        print(f"mean rel error: {dgm_rel_error.mean():.2E}")
+        print(f"std rel error:  {dgm_rel_error.std():.2E}")
+        # Plot
+        plt.plot(grid_1d, dgm, label="Deep Galerkin")
+        # save into csv
+        if fname is not None:
+            plt_data = np.concatenate((plt_data, np.expand_dims(dgm, axis=-1)), axis=-1)
+            plt_header += ", dgm"
+            data = np.stack(
+                (dgm_runtime, dgm_l1_error, dgm_l2_error, dgm_rel_error), axis=-1
+            )
+            np.savetxt(
+                "../logs/data/dgm_" + fname + ".csv",
+                data,
+                delimiter=",",
+                header="runtime,l1_error,l2_error,rel_error",
+                comments="",
+            )
+
+    ######### Deep BSDE method
+    if not disable_bsde:
+        bsde /= len(seeds)
+        bsde_runtime, bsde_l1_error, bsde_l2_error, bsde_rel_error = (
+            np.array(bsde_runtime),
+            np.array(bsde_l1_error),
+            np.array(bsde_l2_error),
+            np.array(bsde_rel_error),
+        )
+        print("\n")
+        print("######## Summary for deep BSDE method")
+        print(f"runtime:        {bsde_runtime.mean():.0f}")
+        print(f"mean L1 error:  {bsde_l1_error.mean():.2E}")
+        print(f"std L1 error:   {bsde_l1_error.std():.2E}")
+        print(f"mean L2 error:  {bsde_l2_error.mean():.2E}")
+        print(f"std L2 error:   {bsde_l2_error.std():.2E}")
+        print(f"mean rel error: {bsde_rel_error.mean():.2E}")
+        print(f"std rel error:  {bsde_rel_error.std():.2E}")
+        # Plot
+        plt.plot(grid_1d, bsde, label="Deep BSDE")
+        # save into csv
+        if fname is not None:
+            plt_data = np.concatenate(
+                (plt_data, np.expand_dims(bsde, axis=-1)), axis=-1
+            )
+            plt_header += ", bsde"
+            data = np.stack(
+                (bsde_runtime, bsde_l1_error, bsde_l2_error, bsde_rel_error), axis=-1
+            )
+            np.savetxt(
+                "../logs/data/bsde_" + fname + ".csv",
+                data,
+                delimiter=",",
+                header="runtime,l1_error,l2_error,rel_error",
+                comments="",
+            )
+
+    plt.legend()
+    plt.show()
+    f.savefig(f"../logs/plot/{'comparison' if fname is None else fname}.pdf")
+    if fname is not None:
+        np.savetxt(
+            "../logs/data/plt_" + fname + ".csv", plt_data, delimiter=",", header=plt_header, comments=""
+        )
